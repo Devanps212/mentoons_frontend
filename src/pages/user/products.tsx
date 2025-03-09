6f5fc6d5274ae4ffda8ec5e4fc7172686acde00a
Devanps <devanps212@gmail.com>
2025-03-09 15:38:08 +0530

    product page started

---
+++ src/pages/user/products.tsx
@@ -0,0 +1,13 @@
+import ProductsBanner from "../../components/home/products/banner";
+import ChallengesFaced from "../../components/home/products/challengesFaced";
+
+const products = () => {
+  return (
+    <>
+      <ProductsBanner />
+      <ChallengesFaced />
+    </>
+  );
+};
+
+export default products;
--- src/routes/userRoute/userRoute.tsx
+++ src/routes/userRoute/userRoute.tsx
@@ -3,10 +3,15 @@ import { lazy, Suspense } from "react";
 import Loader from "../../components/loader/loader";
 import Comic from "../../components/home/comic/comic";
 
-const PrimaryHeader = lazy(() => import("../../components/primaryHeader/primaryHeader"));
-const SecondaryHeader = lazy(() => import("../../components/secondaryHeader/secondaryHeader"));
+const PrimaryHeader = lazy(
+  () => import("../../components/primaryHeader/primaryHeader")
+);
+const SecondaryHeader = lazy(
+  () => import("../../components/secondaryHeader/secondaryHeader")
+);
 const Home = lazy(() => import("../../pages/user/home"));
 const Adda = lazy(() => import("../../pages/adda/adda"));
+const Products = lazy(() => import("../../pages/user/products"));
 
 const UserRouter = () => {
   return (
@@ -16,6 +21,7 @@ const UserRouter = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/adda" element={<Adda />} />
+        <Route path="/products" element={<Products />} />
         <Route path="/comic" element={<Comic />} />
       </Routes>
     </Suspense>
